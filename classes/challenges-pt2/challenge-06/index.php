a8fd3a8507f02e1ba88d09c72a16dc19c72ee0b6
Kevin Wilson <104521683+KevinWilsonAT@users.noreply.github.com>
2025-02-05 22:46:55 -0300

    Chore: Upload class 05-02-25

---
+++ classes/challenges-pt2/challenge-06/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Challenge #6</title>
+</head>
+<body>
+    <?php 
+        $dividend = $_GET['d1'] ?? 0;
+        $divisor = $_GET['d2'] ?? 1;
+    ?>
+    <main>
+        <h1>Division's Antomy</h1>
+        <form action="<?=$_SERVER['PHP_SELF']; ?>" method="get">
+            <label for="d1">Dividend</label>
+            <input type="number" name="d1" id="d1" value="<?=$dividend; ?>">
+            <label for="d2">Divisor</label>
+            <input type="number" name="d2" id="d2" value="<?=$divisor; ?>">
+            <input type="submit" value="Analyze">
+        </form>
+    </main>
+    <section>
+        <h2>Division Structure</h2>
+
+        <?php
+            $quocient = intdiv($dividend, $divisor);
+            $rest = $dividend % $divisor;
+
+            echo "<ul>";
+            echo "<li>Dividend: $dividend</li>";
+            echo "<li>Divisor: $divisor</li>";
+            echo "<li>Quocient: $quocient</li>";
+            echo "<li>Rest: $rest</li>";
+            echo "</ul>";
+        ?>
+
+        <table class="divisao">
+            <tr>
+                <td><?=$dividend; ?></td>
+                <td><?=$divisor; ?></td>
+            </tr>
+            <tr>
+                <td><?=$rest; ?></td>
+                <td><?=$quocient; ?></td>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
---
+++ classes/challenges-pt2/challenge-07/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title>Challenge #7</title>
+</head>
+<body>
+    <?php 
+        $min = 1_380.00;
+        $income = $_GET['inc'] ?? 0;
+    ?>
+    <main>
+        <h1>Inform your income</h1>
+        <form action="<?=$_SERVER['PHP_SELF']; ?>" method="get">
+            <label for="inc">Income (R$)</label>
+            <input type="number" name="inc" id="inc" value="<?=$income; ?>" step="0.01">
+            <p>Consider the minimum R$ <strong><?=number_format($min, 2, ",", "."); ?></strong> </p>
+            <input type="submit" value="Calculate">
+        </form>
+    </main>
+    <section>
+        <h2>Results</h2>
+        <?php
+            $tot = intdiv($income, $min);
+            $dif = $income % $min;
+
+            echo "<p>Your income is $tot minimum wages + R$" . number_format($dif, 2, ",", ".") . "</p>";
+        ?>
+    </section>
+</body>
+</html>
